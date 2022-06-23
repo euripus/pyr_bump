4632d7f36687c7f80fa16e58d0d8ec5da8a349bc
euripus <epistomoclus@gmail.com>
2022-06-23 16:47:05 +0300

    x

--- src/scene/model.cpp
+++ src/scene/model.cpp
@@ -1,1 +1,6 @@
 #include "model.h"
+
+bool ModelSystem::LoadModel(std::string const & fname, ModelComponent & out_mdl)
+{
+    return true;
+}
--- src/scene/model.h
+++ src/scene/model.h
@@ -15,7 +15,7 @@ struct ModelComponent
     struct JointNode
     {
         uint32_t    parent = 0;
-        uint32_t    index = 0;
+        uint32_t    index  = 0;
         std::string name;
 
         std::vector<glm::quat> rot;     // absolute transform matrix for animation
@@ -24,44 +24,43 @@ struct ModelComponent
 
     struct Mesh
     {
-		struct Weight
-		{
-			uint32_t jointIndex = 0;
-			float    w = 0.0f;
-		};
-		using Weights = std::array<Weight, 4>;   // max 4 bones per vertex
-	    
-		//dynamic data
-        std::vector<glm::vec3>              pos;
-        std::vector<glm::vec3>              normal;
-        std::vector<glm::vec3>              tangent;
-        std::vector<glm::vec3>              bitangent;
-		//static data
-		std::vector<Weights>             weights;
-        std::vector<sglm::vec2>     tex_coords;      
-        std::vector<uint32_t> indexes;
-		
-		AABB                                bbox;
+        struct Weight
+        {
+            uint32_t jointIndex = 0;
+            float    w          = 0.0f;
+        };
+        using Weights = std::array<Weight, 4>;   // max 4 bones per vertex
+
+        // dynamic data
+        std::vector<glm::vec3> pos;
+        std::vector<glm::vec3> normal;
+        std::vector<glm::vec3> tangent;
+        std::vector<glm::vec3> bitangent;
+        // static data
+        std::vector<Weights>   weights;
+        std::vector<glm::vec2> tex_coords;
+        std::vector<uint32_t>  indexes;
+
+        evnt::AABB bbox;
     };
 
-	std::vector<Mesh>  meshes;
-	
-	// animation
-    std::vector<JointNode> skeleton;
-    std::vector<AABB>      bboxes;        // size() == num_frames, one bbox at least
-    uint32_t               num_frames = 0;
-    float                  frame_rate = 0.0f;
+    std::vector<Mesh> meshes;
+
+    // animation
+    std::vector<JointNode>  skeleton;
+    std::vector<evnt::AABB> bboxes;   // size() == num_frames, one bbox at least
+    uint32_t                num_frames = 0;
+    float                   frame_rate = 0.0f;
 };
 
 class ModelSystem : public ISystem
 {
 public:
     static ModelComponent GetDefaultModelComponent() { return {}; }
-	static bool LoadModel(std::string const & fname, ModelComponent & out_mdl);
+    static bool           LoadModel(std::string const & fname, ModelComponent & out_mdl);
 
     void        update(Registry & reg, float time_delta);
     std::string getName() const { return "ModelSystem"; }
 };
-
 
 #endif
